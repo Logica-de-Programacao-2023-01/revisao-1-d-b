104a4bf8f78ec3e8cce185bf16e7abb2f73d7d89
Dan1el005 <126472522+Dan1el005@users.noreply.github.com>
2023-04-17 10:26:38 -0300

    q2_resolution

--- q2/q2.go
+++ q2/q2.go
@@ -1,6 +1,18 @@
 package q2
 
 func AverageLettersPerWord(text string) (float64, error) {
-	// Implemente sua solução aqui
-	return 0, nil
+
+	words := strings.Split(text, " ")
+
+	totalletter := 0
+
+	for _, word := range words {
+		totalletter += len(word)
+
+	}
+
+	average := float64(totalletter) / float64(len(words))
+
+	return average, nil
 }
+
