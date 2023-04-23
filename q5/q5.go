209e38958f4d3dd798822d7b452dbd14fd8c8c01
Daniel Assunção Bouças <126472522+Dan1el005@users.noreply.github.com>
2023-04-23 00:01:24 -0300

    Update q5.go

--- q5/q5.go
+++ q5/q5.go
@@ -1,25 +1,26 @@
 package q5
 
 func ConvertTemperature(temp float64, fromScale string, toScale string) (float64, error) {
-
-	var err error
-
-	if fromScale == "C" && toScale == "F" {
-		convert := temp*9/5 + 32
-		return convert, err
-	} else if fromScale == "C" && toScale == "K" {
-		convert := temp + 273.15
-		return convert, err
-	} else if fromScale == "F" && toScale == "C" {
-		convert := (temp - 32) * 5 / 9
-		return convert, err
-	} else if fromScale == "F" && toScale == "K" {
-		convert := (temp-32)*5/9 + 273.15
-		return convert, err
-	} else if fromScale == "K" && toScale == "C" {
-		convert := temp - 273.15
-		return convert, err
-	} else {
-		convert := (temp-273.15)*9/5 + 32
-		return convert, err
-	}
+		var err error
+	
+		if fromScale == "C" && toScale == "F" {
+			convert := temp*9/5 + 32
+			return convert, err
+		} else if fromScale == "C" && toScale == "K" {
+			convert := temp + 273.15
+			return convert, err
+		} else if fromScale == "F" && toScale == "C" {
+			convert := (temp - 32) * 5 / 9
+			return convert, err
+		} else if fromScale == "F" && toScale == "K" {
+			convert := (temp-32)*5/9 + 273.15
+			return convert, err
+		} else if fromScale == "K" && toScale == "C" {
+			convert := temp - 273.15
+			return convert, err
+		} else if fromScale == "K" && toScale == "F" {
+			convert := (temp-273.15)*9/5 + 32
+			return convert, err
+		}else if err != nil {
+		fmt.Println("escala inválida")
+}
