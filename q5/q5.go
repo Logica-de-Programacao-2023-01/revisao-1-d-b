fe7d936fc3ed396a211652bab7f57e497aa740c9
Daniel Assunção Bouças <126472522+Dan1el005@users.noreply.github.com>
2023-04-23 00:22:02 -0300

    Update q5.go

--- q5/q5.go
+++ q5/q5.go
@@ -1,28 +1,35 @@
 package q5
 
 func ConvertTemperature(temp float64, fromScale string, toScale string) (float64, error) {
-	var err error
-	var convert float64
-
-	if fromScale == "C" && toScale == "F" {
-		convert := temp*9/5 + 32
-		return convert, err
-	} else if fromScale == "C" && toScale == "K" {
-		convert := temp + 273.15
-		return convert, err
-	} else if fromScale == "F" && toScale == "C" {
-		convert := (temp - 32) * 5 / 9
-		return convert, err
-	} else if fromScale == "F" && toScale == "K" {
-		convert := (temp-32)*5/9 + 273.15
-		return convert, err
-	} else if fromScale == "K" && toScale == "C" {
-		convert := temp - 273.15
-		return convert, err
-	} else if fromScale == "K" && toScale == "F" {
-		convert := (temp-273.15)*9/5 + 32
-		return convert, err
-	} else {
-		return convert, err
-	}
+    switch from {
+    case Celsius:
+        switch to {
+        case Fahrenheit:
+            return temp*9/5 + 32, nil
+        case Kelvin:
+            return temp + 273.15, nil
+        default:
+            return 0, fmt.Errorf("escala de destino inválida")
+        }
+    case Fahrenheit:
+        switch to {
+        case Celsius:
+            return (temp - 32) * 5 / 9, nil
+        case Kelvin:
+            return (temp-32)*5/9 + 273.15, nil
+        default:
+            return 0, fmt.Errorf("escala de destino inválida")
+        }
+    case Kelvin:
+        switch to {
+        case Celsius:
+            return temp - 273.15, nil
+        case Fahrenheit:
+            return (temp-273.15)*9/5 + 32, nil
+        default:
+            return 0, fmt.Errorf("escala de destino inválida")
+        }
+    default:
+        return 0, fmt.Errorf("escala de origem inválida")
+    }
 }
